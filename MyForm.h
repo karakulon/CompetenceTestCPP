3c602227fb0cbe2aa185774201f70ec5873612f1
karakulon <karakulon22@gmail.com>
2024-05-16 23:24:17 +0300

    wp

--- MyForm.h
+++ MyForm.h
@@ -26,6 +26,7 @@ namespace CompetenceTest {
 		int counter = 0;
 		List<Question^>^ collection = gcnew List<Question^>();
 	private: System::Windows::Forms::Button^ btn_exit;
+	private: System::Windows::Forms::Button^ btn_admin;
 	public:
 		User^ user = gcnew User("", 0, 0);
 		MyForm(void)
@@ -39,7 +40,6 @@ namespace CompetenceTest {
 			collection->Add(gcnew Question("В каком году появился C++", "A) 1964\n\nB) 1980\n\nC) 1984\n\nD) 1983", "A", "B", "C", "D", 'D', 0, 'm'));
 			collection->Add(gcnew Question("Что быстрее  преобразует код в native", "A) Интерпретатор\n\nB) Компилятор\n\nC) Одинаково\n\nD) Ни один из них", "A", "B", "C", "D", 'B', 0, 'e'));
 			collection->Add(gcnew Question("Что такое ООП", "A) Область определения площади\n\nB) Объектно-ориентрованное программирование\n\nC) Ориентированное-объектовое программировние\n\nD) Не знаю", "A", "B", "C", "D", 'B', 0, 'e'));
-			collection->Add(gcnew Question("Что такое ООП", "A) Область определения площади\n\nB) Объектно-ориентрованное программирование\n\nC) Ориентированное-объектовое программировние\n\nD) Не знаю", "A", "B", "C", "D", 'B', 0, 'h'));
 		}
 
 	protected:
@@ -89,7 +89,7 @@ namespace CompetenceTest {
 		/// </summary>
 		/// 
 		System::ComponentModel::Container ^components;
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ score_label;
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Требуемый метод для поддержки конструктора — не изменяйте 
@@ -107,8 +107,9 @@ namespace CompetenceTest {
 			this->btn_start = (gcnew System::Windows::Forms::Button());
 			this->input_username_box = (gcnew System::Windows::Forms::TextBox());
 			this->start_label = (gcnew System::Windows::Forms::Label());
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->score_label = (gcnew System::Windows::Forms::Label());
 			this->btn_exit = (gcnew System::Windows::Forms::Button());
+			this->btn_admin = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// btn_answer_A
@@ -197,10 +198,10 @@ namespace CompetenceTest {
 			// 
 			// input_username_box
 			// 
-			this->input_username_box->Location = System::Drawing::Point(257, 262);
+			this->input_username_box->Location = System::Drawing::Point(291, 263);
 			this->input_username_box->Multiline = true;
 			this->input_username_box->Name = L"input_username_box";
-			this->input_username_box->Size = System::Drawing::Size(259, 50);
+			this->input_username_box->Size = System::Drawing::Size(247, 35);
 			this->input_username_box->TabIndex = 13;
 			// 
 			// start_label
@@ -209,24 +210,25 @@ namespace CompetenceTest {
 			this->start_label->AutoSize = true;
 			this->start_label->BackColor = System::Drawing::SystemColors::Control;
 			this->start_label->ForeColor = System::Drawing::SystemColors::ControlText;
-			this->start_label->Location = System::Drawing::Point(295, 218);
+			this->start_label->Location = System::Drawing::Point(327, 228);
 			this->start_label->Name = L"start_label";
 			this->start_label->Size = System::Drawing::Size(247, 32);
 			this->start_label->TabIndex = 14;
 			this->start_label->Text = L"введите свое имя";
 			// 
-			// label1
+			// score_label
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(150, 120);
-			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(0, 32);
-			this->label1->TabIndex = 15;
+			this->score_label->AutoSize = true;
+			this->score_label->Location = System::Drawing::Point(150, 120);
+			this->score_label->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->score_label->Name = L"score_label";
+			this->score_label->Size = System::Drawing::Size(0, 32);
+			this->score_label->TabIndex = 15;
 			// 
 			// btn_exit
 			// 
 			this->btn_exit->BackColor = System::Drawing::Color::Crimson;
+			this->btn_exit->Cursor = System::Windows::Forms::Cursors::Hand;
 			this->btn_exit->Location = System::Drawing::Point(757, 11);
 			this->btn_exit->Margin = System::Windows::Forms::Padding(2);
 			this->btn_exit->Name = L"btn_exit";
@@ -236,16 +238,29 @@ namespace CompetenceTest {
 			this->btn_exit->UseVisualStyleBackColor = false;
 			this->btn_exit->Click += gcnew System::EventHandler(this, &MyForm::btn_exit_Click);
 			// 
+			// btn_admin
+			// 
+			this->btn_admin->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_admin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btn_admin->Location = System::Drawing::Point(664, 460);
+			this->btn_admin->Name = L"btn_admin";
+			this->btn_admin->Size = System::Drawing::Size(164, 65);
+			this->btn_admin->TabIndex = 17;
+			this->btn_admin->Text = L"продолжить";
+			this->btn_admin->UseVisualStyleBackColor = true;
+			this->btn_admin->Visible = false;
+			this->btn_admin->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::btn_admin_MouseClick);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->AutoSize = true;
 			this->BackColor = System::Drawing::SystemColors::Control;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->ClientSize = System::Drawing::Size(840, 537);
+			this->Controls->Add(this->btn_admin);
 			this->Controls->Add(this->btn_exit);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->score_label);
 			this->Controls->Add(this->start_label);
 			this->Controls->Add(this->input_username_box);
 			this->Controls->Add(this->btn_start);
@@ -260,6 +275,7 @@ namespace CompetenceTest {
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Competence test";
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -268,10 +284,10 @@ namespace CompetenceTest {
 #pragma endregion
 
 private: System::Void btn_answer_A_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	if (counter >= 5) 
+	if (counter >= collection->Count - 1)
 	{
-		this->label1->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + counter.ToString();
-		this->label1->Visible = true;
+		this->score_label->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + collection->Count.ToString();
+		this->score_label->Visible = true;
 		this->btn_answer_A->Visible = false;
 		this->btn_answer_B->Visible = false;
 		this->btn_answer_C->Visible = false;
@@ -283,7 +299,6 @@ private: System::Void btn_answer_A_MouseClick(System::Object^ sender, System::Wi
 	{
 		if (this->btn_answer_A->Text[0] == collection[counter]->trueSolution)
 		{
-			this->btn_exit->Text = "V";
 			user->score++;
 		}
 		counter++;
@@ -292,10 +307,10 @@ private: System::Void btn_answer_A_MouseClick(System::Object^ sender, System::Wi
 	}
 }
 private: System::Void btn_answer_B_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	if (counter >= 5)
+	if (counter >= collection->Count - 1)
 	{
-		this->label1->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + counter.ToString();
-		this->label1->Visible = true;
+		this->score_label->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + collection->Count.ToString();;
+		this->score_label->Visible = true;
 		this->btn_answer_A->Visible = false;
 		this->btn_answer_B->Visible = false;
 		this->btn_answer_C->Visible = false;
@@ -307,7 +322,6 @@ private: System::Void btn_answer_B_MouseClick(System::Object^ sender, System::Wi
 	{
 		if (this->btn_answer_B->Text[0] == collection[counter]->trueSolution)
 		{
-			this->btn_exit->Text = "V";
 			user->score++;
 		}
 		counter++;
@@ -316,10 +330,10 @@ private: System::Void btn_answer_B_MouseClick(System::Object^ sender, System::Wi
 	}
 }
 private: System::Void btn_answer_C_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	if (counter >= 5)
+	if (counter >= collection->Count - 1)
 	{
-		this->label1->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + counter.ToString();
-		this->label1->Visible = true;
+		this->score_label->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + collection->Count.ToString();;
+		this->score_label->Visible = true;
 		this->btn_answer_A->Visible = false;
 		this->btn_answer_B->Visible = false;
 		this->btn_answer_C->Visible = false;
@@ -331,7 +345,6 @@ private: System::Void btn_answer_C_MouseClick(System::Object^ sender, System::Wi
 	{
 		if (this->btn_answer_C->Text[0] == collection[counter]->trueSolution)
 		{
-			this->btn_exit->Text = "V";
 			user->score++;
 		}
 		counter++;
@@ -340,10 +353,10 @@ private: System::Void btn_answer_C_MouseClick(System::Object^ sender, System::Wi
 	}
 }
 private: System::Void btn_answer_D_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	if (counter >= 5)
+	if (counter >= collection->Count - 1)
 	{
-		this->label1->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + counter.ToString();
-		this->label1->Visible = true;
+		this->score_label->Text = user->nickname + "  \n" + "Ваш результат\n" + user->score.ToString() + "/" + collection->Count.ToString();;
+		this->score_label->Visible = true;
 		this->btn_answer_A->Visible = false;
 		this->btn_answer_B->Visible = false;
 		this->btn_answer_C->Visible = false;
@@ -355,7 +368,6 @@ private: System::Void btn_answer_D_MouseClick(System::Object^ sender, System::Wi
 	{
 		if (this->btn_answer_D->Text[0] == collection[counter]->trueSolution)
 		{
-			this->btn_exit->Text = "V";
 			user->score++;
 		}
 		counter++;
@@ -363,34 +375,52 @@ private: System::Void btn_answer_D_MouseClick(System::Object^ sender, System::Wi
 		this->Question_label->Text = collection[counter]->mainquestion;
 	}
 }
-private: System::Void btn_start_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-	if(this->input_username_box->Text->Length > 12)
-	{
-		this->start_label->Text = L"имя слишком длинное";
-		this->start_label->BackColor = System::Drawing::SystemColors::MenuHighlight;
+	private: System::Void btn_start_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		if (this->input_username_box->Text == "admin")
+		{
+			this->btn_start->Visible = false;
+			this->btn_admin->Visible = true;
+			this->input_username_box->Text = "";
+			this->start_label->Text = "введите пароль";
+		}
+		else
+		{
+			if (this->input_username_box->Text->Length > 12)
+			{
+				this->start_label->Text = L"имя слишком длинное";
+				this->start_label->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			}
+			if (this->input_username_box->Text->Length <= 3)
+			{
+				this->start_label->Text = L"имя слишком короткое";
+				this->start_label->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			}
+			if (this->input_username_box->Text->Length > 3 && this->input_username_box->Text->Length <= 12)
+			{
+				user->nickname = this->input_username_box->Text;
+				this->start_label->Visible = false;
+				this->input_username_box->Visible = false;
+				this->btn_start->Enabled = false;
+				this->btn_start->Visible = false;
+				this->btn_answer_A->Visible = true;
+				this->btn_answer_B->Visible = true;
+				this->btn_answer_C->Visible = true;
+				this->btn_answer_D->Visible = true;
+				this->VariousOfAnswers_label->Visible = true;
+				this->Question_label->Visible = true;
+				this->Question_label->Text = collection[counter]->mainquestion;
+				this->VariousOfAnswers_label->Text = collection[counter]->description;
+			}
+		}
 	}
-	if (this->input_username_box->Text->Length <= 3)
-	{
-		this->start_label->Text = L"имя слишком короткое";
-		this->start_label->BackColor = System::Drawing::SystemColors::MenuHighlight;
+	private: System::Void btn_admin_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		if (input_username_box->Text != "1111") 
+		{
+			this->start_label->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->start_label->Text = "неверный пороль";
+		}
 	}
-	if (this->input_username_box->Text->Length > 3 && this->input_username_box->Text->Length <= 12)
-	{		
-		user->nickname = this->input_username_box->Text;
-		this->start_label->Visible = false;
-		this->input_username_box->Visible = false;
-		this->btn_start->Enabled = false;
-		this->btn_start->Visible = false;
-		this->btn_answer_A->Visible = true;
-		this->btn_answer_B->Visible = true;
-		this->btn_answer_C->Visible = true;
-		this->btn_answer_D->Visible = true;
-		this->VariousOfAnswers_label->Visible = true;
-		this->Question_label->Visible = true;
-		this->VariousOfAnswers_label->Text = collection[counter]->description;
-	}
-}
-	private: System::Void btn_exit_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void btn_exit_Click(System::Object ^ sender, System::EventArgs ^ e) {
 		this->Close();
 	}
 };
